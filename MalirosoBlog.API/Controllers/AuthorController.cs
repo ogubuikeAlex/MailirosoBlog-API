78f63e2e8d1a3e19b03172d38f96b272bd05534c
king-Alex-d-great <ogubuikealex@gmail.com>
2024-03-29 07:13:36 +0100

    Feat: Add Author Controller

---
+++ MalirosoBlog.API/Controllers/AuthorController.cs
@@ -0,0 +1,49 @@
+﻿using MalirosoBlog.Models.DTO.Request;
+using MalirosoBlog.Models.DTO.Response;
+using MalirosoBlog.Services.Implementation;
+using MalirosoBlog.Services.Infrastructure;
+using MalirosoBlog.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MalirosoBlog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Policy = "AdminOnly")]
+    [ApiController]
+    [SwaggerTag("Author Based Operations")]
+    public class AuthorController(IAuthorService authorService) : BaseController
+    {
+        private readonly IAuthorService _authorService = authorService;
+
+        [HttpGet("{id}", Name = "Get-Author")]
+        [SwaggerOperation(Summary = "Get An Author By Their Id")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Author Retrieved", Type = typeof(AuthorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "No record found", Type = typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "It's not you, it's us", Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> GetAuthor(string id)
+        {
+            return Ok(await _authorService.Get(id));
+        }
+
+        [HttpGet(Name = "Get-All-Authors")]
+        [SwaggerOperation(Summary = "Get All Authors")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Authors Retrieved", Type = typeof(PagedList<AuthorResponse>))]        
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "It's not you, it's us", Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> GellAllAuthors([FromQuery]AuthorRequestParameter request)
+        {
+            return Ok(await _authorService.Get(request));
+        }
+
+        [HttpPut("{id}", Name = "Delete-Author")]
+        [SwaggerOperation(Summary = "Soft Delete An Author")]
+        [SwaggerResponse(StatusCodes.Status200OK, Description = "Author Soft Deleted", Type = typeof(AuthorResponse))]
+        [SwaggerResponse(StatusCodes.Status404NotFound, Description = "No record found", Type = typeof(ErrorResponse))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "It's not you, it's us", Type = typeof(ErrorResponse))]
+        public async Task<IActionResult> SoftDeleteAuthor(string id)
+        {
+            return Ok(await _authorService.Delete(id));
+        }
+    }
+}
---
+++ MalirosoBlog.API/Controllers/BaseController.cs
@@ -0,0 +1,107 @@
+﻿using MalirosoBlog.Models.Entities;
+using MalirosoBlog.Services.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Security.Claims;
+
+namespace MalirosoBlog.API.Controllers
+{
+    [SwaggerResponse(StatusCodes.Status400BadRequest, Type = typeof(ErrorResponse))]
+    [SwaggerResponse(StatusCodes.Status403Forbidden, Description = "User is forbidden", Type = typeof(ErrorResponse))]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "User is unauthorized", Type = typeof(ErrorResponse))]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "It's not you, it's us", Type = typeof(ErrorResponse))]
+    [Produces("application/json")]
+    public class BaseController : ControllerBase
+    {
+        protected readonly UserManager<ApplicationUser> _userManager;
+        public BaseController()
+        {
+
+        }
+
+        public BaseController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public override OkObjectResult Ok(object value)
+        {
+            SuccessResponse response = new()
+            {
+                Data = value,
+                Success = true
+            };
+
+            return base.Ok(response);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public new OkObjectResult Ok()
+        {
+            SuccessResponse response = new()
+            {
+                Success = true
+            };
+
+            return base.Ok(response);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public NotFoundObjectResult NotFound()
+        {
+            SuccessResponse response = new()
+            {
+                Success = false,
+            };
+
+            return base.NotFound(response);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public OkObjectResult Invalid()
+        {
+            SuccessResponse response = new()
+            {
+
+                Success = false
+            };
+
+            /*return invalid */
+            return base.Ok(response);
+
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return await _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<string> GetUserIdAsync()
+        {
+            ApplicationUser user = await GetCurrentUserAsync();
+            return user.Id;
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public string GetUserId()
+        {
+            string userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return userId;
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public OkObjectResult Error()
+        {
+            SuccessResponse response = new()
+            {
+                Success = false
+            };
+
+            return base.Ok(response);
+        }
+    }
+}
